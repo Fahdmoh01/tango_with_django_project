3f4bd958f4dde84da16d7e64bb3893b096ca81b0
Fahd Mohammed <fahdmoh.1@gmail.com>
2023-01-27 12:10:42 +0000

    all test passed. Chapter 6 completed

--- rango/admin.py
+++ rango/admin.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 from rango.models import Category, Page
 
 
+#helps to automatic slugify names
+class CategoryAdmin(admin.ModelAdmin):
+	prepopulated_fields={'slug':('name',)}
+
+#This helps displays tile, category and url in the admin panel
 class PageAdmin(admin.ModelAdmin):
 	list_display=('title','category', 'url')
 
 # Register your models here.
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
+#This registers and displays tile, category and url in the admin panel
 admin.site.register(Page,PageAdmin)
--- rango/models.py
+++ rango/models.py
@@ -1,10 +1,23 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 # Create your models here.
 class Category(models.Model):
 	name = models.CharField(max_length=128, unique=True)
 	views= models.IntegerField(default=0)
 	likes = models.IntegerField(default=0)
+	
+	#adding a slug field to slugify URLS.
+	#always add the unique attribute after following creating your Database and populating it.
+	#else there will be migration issues.
+	slug = models.SlugField(unique=True) 
+
+
+	def save(self, *args, **kwargs):
+		self.slug = slugify(self.name)
+		super(Category, self).save(*args, **kwargs)
+
+
 	#Meta class just allows for correct plural form of model names in Django admin panel
 	class Meta:
 		verbose_name_plural = 'Categories'
